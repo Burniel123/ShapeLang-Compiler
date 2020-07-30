7221d43da29ccb95e867c6a58e0784d93d115f32
Mike <michael.hallam3@gmail.com>
2020-07-30 18:02:24 +0100

    Initial parser commit

---
+++ src/main/java/org/tokeniser/parser.java
@@ -0,0 +1,13 @@
+public class Parser
+{
+	private final static String newPara = "\u0x0a" // TODO - find actual utf 16 for paragraph break
+	private static tokenise(String toTokenise) throws BadSyntaxException
+	{
+		String consume = toTokenise;
+		int pos = 0;
+		while(pos < consume.length)
+		{
+
+		}
+	}
+}
