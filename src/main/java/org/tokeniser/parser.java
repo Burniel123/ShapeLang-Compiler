505191037e3feed6fd1993d5156b522dba0421dd
Mike <michael.hallam3@gmail.com>
2020-07-31 13:50:35 +0100

    Added basic tokens. Still unsure of how to communicate tokenisation

--- src/main/java/org/tokeniser/parser.java
+++ src/main/java/org/tokeniser/parser.java
@@ -1,13 +1,51 @@
+class TokeniseException extends Exception
+{
+	TokeniseException(String s)
+	{
+		println("You're probably missing a space or linebreak 
+				"somewhere");
+		println("See the below stack trace for more info (line number 
+			"tends to be very helpful");
+
+		super(s);
+	}
+}
+
+public enum Tokens
+{
+	// instantiation
+	PUT,
+	COORD,
+	2SIZE, // TODO - how to tokenise actual type size
+	// control flow
+	LOOP,
+	FOR,
+	ENDLOOP, // alias end of loops to same token
+	SEQ,
+	ENDSEQ,
+	BLOCK,
+	TIME,
+	// modify shape 
+	MOVE,
+	RESIZE,
+	ROTATE,
+}
+
 public class Parser
 {
 	private final static String newPara = "\u0x0a" // TODO - find actual utf 16 for paragraph break
-	private static tokenise(String toTokenise) throws BadSyntaxException
+	private static tokenise(String toTokenise) throws TokeniseException
 	{
-		String consume = toTokenise;
-		int pos = 0;
-		while(pos < consume.length)
+		String[] consume = toTokenise.split(newPara); // split into array
+		
+		for(String cur: consume) // iterate over consume
 		{
-
+			switch(cur)
+			{
+				case "
+			}
 		}
 	}
+
+	private static 
 }
