1fea265bf85d75e86475bc6f5b73efe6fb01dd89
Mike <michael.hallam3@gmail.com>
2020-08-01 12:00:36 +0100

    Moved Twople to its own class to avoid inner classes

---
+++ src/main/java/org/com/Twople.java
@@ -0,0 +1,15 @@
+package Common;
+
+public class Twople<U,V> 
+{ 
+	// immutable because that's the standard in many languages. Make a new
+	// tuple if you want to change state
+	public final U u;
+	public final V v;
+
+	public Twople(U u, V v) {
+		this.u = u;
+		this.v = v;
+	}
+}
+
