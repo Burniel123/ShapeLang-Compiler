247c48dadbc601ba2b987f163c33fbc149f6a2cc
Mike <michael.hallam3@gmail.com>
2020-08-03 22:57:11 +0100

    Began the basics of the parser. Added in canvinit and basic outline of the main bulk of parser

--- src/main/java/org/tokeniser/parser.java
+++ src/main/java/org/tokeniser/parser.java
@@ -1,51 +1,76 @@
-class TokeniseException extends Exception
-{
-	TokeniseException(String s)
-	{
-		println("You're probably missing a space or linebreak 
-				"somewhere");
-		println("See the below stack trace for more info (line number 
-			"tends to be very helpful");
+import com.Twople;
+import com.parsercom.*;
+// as far as possible, this parser is functional
+// some loops will be found. This is for efficiency reasons
+// this could be done entirely functionally, but this would involve recursion
 
-		super(s);
-	}
-}
 
-public enum Tokens
-{
-	// instantiation
-	PUT,
-	COORD,
-	2SIZE, // TODO - how to tokenise actual type size
-	// control flow
-	LOOP,
-	FOR,
-	ENDLOOP, // alias end of loops to same token
-	SEQ,
-	ENDSEQ,
-	BLOCK,
-	TIME,
-	// modify shape 
-	MOVE,
-	RESIZE,
-	ROTATE,
-}
 
 public class Parser
 {
-	private final static String newPara = "\u0x0a" // TODO - find actual utf 16 for paragraph break
-	private static tokenise(String toTokenise) throws TokeniseException
+
+	private final static String newPara = "\u0x0a"; // TODO - find actual utf 16 for paragraph break
+	private final static String CANV_INIT_ERR = "Initialise canvas with <size> could not be found.\nPlease ensure this is at the top of the file!";
+	private final static String CMD_ERR = "Error deciphering following text: ";
+
+	public static CanvasInit tokenise(String toTokenise) throws TokeniseException
 	{
-		String[] consume = toTokenise.split(newPara); // split into array
+		final String lc = toTokenise.toLowerCase();
+		final String fst = lc.dropWhileNEQ("initialise");
+		final String[] lines = fst.splitAt(newPara);
+		// TODO - extend this so Initialise doesn't have to be on literally the first line
+		final String words[] = wordify(lines);
+
+		switch (words[0]) {
+			case "initialise" : 
+				final CanvasInit ci = new CanvasInit();
+				ci.size = sizeString(words[2]);
+				cs.next = tokenise(Arrays.copyOfRange(words,1,words.length());
+				return ci;
+				break;
+			default: throw new TokeniseException(CANV_INIT_ERR);
+				break;
+		}
+
+		return null; // it shouldn't ever hit this point
+	}
 		
-		for(String cur: consume) // iterate over consume
+	
+
+	private static [] tokenise(String[] lines) throws TokeniseException
+	{
+		final Text head = new Text();
+		Text cur = head; // this is part of our loop variant. This will 
+		// change so as to avoid recursion
+
+		for(String line: lines)
 		{
-			switch(cur)
-			{
-				case "
-			}
+			switch(lines[0]) {
+				case "Put" :
+
+					break;
+				default: throw new TokeniseException(CMD_ERR+lines[0]);
 		}
 	}
 
-	private static 
+
+	private static String[] wordify(String line){
+		return line.splitAt(" ");
+	}
+
+	private static Twople<Integer,Integer> sizeString(String tuple) {
+		// specific format: [some stuff]"("[num1],[num2]")"[some stuff] -> ([num],[num])
+		while(
+	}
+
+	private static dropWhileNEQ(String toDrop, String eq)
+	{ // TODO - investigate if built in method exists
+		int count = 0;
+		
+		while(toDrop.length() - eq.length > count && 
+			eq != toDrop.substring(count,count+eq.length()))
+				count++;
+
+		return toDrop.substring(count);
+	}
 }
