3b234dd5057e352f1164229ddfef6f0ae039004c
Mike <michael.hallam3@gmail.com>
2020-08-04 13:47:20 +0100

    Switch now contains all leading words, and implementations for some of them

--- src/main/java/org/tokeniser/parser.java
+++ src/main/java/org/tokeniser/parser.java
@@ -12,6 +12,7 @@ public class Parser
 	private final static String newPara = "\u0x0a"; // TODO - find actual utf 16 for paragraph break
 	private final static String CANV_INIT_ERR = "Initialise canvas with <size> could not be found.\nPlease ensure this is at the top of the file!";
 	private final static String CMD_ERR = "Error deciphering following text: ";
+	private final static String LOOP_OOB_ERR = "Error with number in for loop.\nCheck your for loops have valid numbers";
 
 	public static CanvasInit tokenise(String toTokenise) throws TokeniseException
 	{
@@ -25,7 +26,7 @@ public class Parser
 			case "initialise" : 
 				final CanvasInit ci = new CanvasInit();
 				ci.size = sizeString(words[2]);
-				cs.next = tokenise(Arrays.copyOfRange(words,1,words.length());
+				ci.next = tokenise(Arrays.copyOfRange(words,1,words.length()).fst;
 				return ci;
 				break;
 			default: throw new TokeniseException(CANV_INIT_ERR);
@@ -35,24 +36,100 @@ public class Parser
 		return null; // it shouldn't ever hit this point
 	}
 		
-	
-
-	private static [] tokenise(String[] lines) throws TokeniseException
+	// when it calls itself for a loop, pos in loop required
+	private static Twople<Text,Integer> tokenise(String[] lines) throws TokeniseException
 	{
 		final Text head = new Text();
 		Text cur = head; // this is part of our loop variant. This will 
 		// change so as to avoid recursion
+		int count = 0;
 
-		for(String line: lines)
+		while(count < lines.length)
 		{
-			switch(lines[0]) {
-				case "Put" :
+			final String[] line = lines[count].splitAt(" ");
+			final Statement curAct; // current action
 
+			switch(line[0]) {
+				case "put":
+					
+					break;
+				case "move":
+					break;
+				case "resize":
+					break;
+				case "loop":
+					final Loop loop = new Loop();
+
+					loop.numIter = Optional.empty();
+
+					final Twople<Text,Integer> res = 
+						loopify(lines,count);
+					loop.contents = twople.fst;
+					count = twople.snd;
+					
+					curAct = loop;
+					break;
+				case "for":
+					final Loop loop = new Loop();
+					
+					loop.numIter = 
+						Optional.of(loopIterify(line));
+					if(0 > loop.numIter)
+						throw new TokeniseException(LOOP_OOB_ERR);
+
+					final Twople<Text,Integer> res =
+						loopify(lines,count);
+					loop.contents = twople.fst;
+					count = twople.snd;
+
+					curAct = loop;
+					break;
+				case "endloop":
+					return new Twople(head,count);
+					break;
+				case "endfor":
+					return new Twople(head,count);
+					break;
+				case "block":
+					break;
+				case "sequential":
+					break;
+				case "endsequential":
 					break;
 				default: throw new TokeniseException(CMD_ERR+lines[0]);
+					break;
+			}
+
+			cur.stmt = curAct;
+
+			// move onto next line
+			final Text nxt = new Text();
+			cur.next = nxt;
+			cur = cur.next;
+			count++;
 		}
+
+		return new Twople(head,count);
 	}
 
+	private static int loopIterify(String[] line) {
+		boolean next = false;
+		for(String word: line) {
+			switch (next) {
+				case true: return Integer.parseInt(line);
+					 break;
+				case false: next = line.equals("do");
+					 break;
+			}
+		}
+
+		return -1;
+	}
+
+	private static Twople<Text,Integer> loopify(String[] lines, int count) {
+		return tokenise(lines.copyOfRange
+				(lines, count+1, lines.length);
+	}
 
 	private static String[] wordify(String line){
 		return line.splitAt(" ");
@@ -60,7 +137,33 @@ public class Parser
 
 	private static Twople<Integer,Integer> sizeString(String tuple) {
 		// specific format: [some stuff]"("[num1],[num2]")"[some stuff] -> ([num],[num])
-		while(
+		
+		final int fst;
+		final int snd;
+		int pos = 0;
+		
+		while('(' != tuple.charAt(pos))
+			pos++;
+		String fstDig = "";
+
+		while(',' != tuple.charAt(pos)) {
+			fstDig += tuple.charAt(pos);
+			pos++;
+		}
+		// TODO - account for spaces
+
+		fst = Integer.parseInt(fstDig);
+		pos += 1;
+		String sndDig = "";
+
+		while(')' != tuple.charAt(pos)) {
+			sndDig += tuple.charAt(pos);
+			pos++;
+		}
+
+		snd = Integer.parseInt(sndDig);
+
+		return new Twople(fst,snd);
 	}
 
 	private static dropWhileNEQ(String toDrop, String eq)
