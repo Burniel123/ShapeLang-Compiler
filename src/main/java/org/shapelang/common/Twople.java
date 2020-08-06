d55f50a2ce466cf60c672700f6f2b4120f9038c0
Burniel123 <dgburton12@gmail.com>
2020-08-06 12:05:58 +0100

    Add Twople class from branch parser.

---
+++ src/main/java/org/shapelang/common/Twople.java
@@ -0,0 +1,17 @@
+package org.shapelang.common;
+
+public class Twople<U,V> 
+{ 
+	// immutable because that's the standard in many languages. Make a new
+	// tuple if you want to change state
+	public final U fst;
+	public final V snd;
+
+	public Twople(U fst, V snd) {
+		this.fst = fst;
+		this.snd = snd;
+	}
+
+	// due to type erasure, an equals method cannot be implemented easily
+}
+
