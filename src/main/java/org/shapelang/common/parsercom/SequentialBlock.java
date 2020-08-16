95ebc8b0ec1a2e3499821c6a4e58b7e6ad7ef999
Mike <michael.hallam3@gmail.com>
2020-08-16 12:01:19 +0100

    Added StmtType interface to SequentialBlock

--- src/main/java/org/shapelang/common/parsercom/SequentialBlock.java
+++ src/main/java/org/shapelang/common/parsercom/SequentialBlock.java
@@ -2,8 +2,13 @@ package org.shapelang.common.parsercom;
 
 import org.shapelang.shapes.Shape;
 
-public class SequentialBlock
+public class SequentialBlock implements StmtType
 {
+	@Override
+	public ParserToken stmtType() {
+		return ParserToken.SEQ;
+	}
+
 	public final Shape[] shapes;
 	public final Text text;
 }
