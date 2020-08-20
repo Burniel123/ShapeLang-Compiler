6ff1b8ffdf3435181c45e56f3572ef672274bda3
Mike <michael.hallam3@gmail.com>
2020-08-20 14:15:27 +0100

    Made next non functional to allow for loop user in parser

--- src/main/java/org/shapelang/common/parsercom/Text.java
+++ src/main/java/org/shapelang/common/parsercom/Text.java
@@ -4,11 +4,19 @@ import java.util.Optional;
 
 public class Text
 {
-	public Text(StmtType stmt, Optional<Text> next) {
+	public Text(StmtType stmt) {
 		this.stmt = stmt;
+	}
+
+	public Optional<Text> getNext() {
+		return next;
+	}
+
+	public void setNext(Optional<Text> next) {
 		this.next = next;
 	}
 
 	public final StmtType stmt;
-	public final Optional<Text> next;
+	private Optional<Text> next; // the only non functional part of the tokens
+	// this has been left _purely_ to allow for loop use instead of recursion
 }
