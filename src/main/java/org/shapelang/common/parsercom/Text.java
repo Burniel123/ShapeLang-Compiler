981080bda9ddb723d733e5c608b09089b52a9dbb
Mike <michael.hallam3@gmail.com>
2020-08-20 13:56:28 +0100

    Added constructor to allow for assignment to finals (2 -> 4)

--- src/main/java/org/shapelang/common/parsercom/Text.java
+++ src/main/java/org/shapelang/common/parsercom/Text.java
@@ -4,6 +4,11 @@ import java.util.Optional;
 
 public class Text
 {
+	public Text(StmtType stmt, Optional<Text> next) {
+		this.stmt = stmt;
+		this.next = next;
+	}
+
 	public final StmtType stmt;
 	public final Optional<Text> next;
 }
