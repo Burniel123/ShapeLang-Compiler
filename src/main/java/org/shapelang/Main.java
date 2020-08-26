e1abc5c1dd0e928a02bec93689be501746906941
Burniel123 <dgburton12@gmail.com>
2020-08-26 12:24:23 +0100

    Add a reader at the init() phase to read a file whose path is specified on the command line on startup. Further error handling to be added.

--- src/main/java/org/shapelang/Main.java
+++ src/main/java/org/shapelang/Main.java
@@ -1,9 +1,15 @@
 package org.shapelang;
 
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 import org.shapelang.shapes.SLFreeTriangle;
+import org.shapelang.shapes.SLRegularTriangle;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Main application class for the ShapeLang compiler.
@@ -29,11 +35,15 @@ public class Main extends Application
     public void start(Stage stage)
     {
         SLFreeTriangle ft = new SLFreeTriangle(10,10,100,100,100,200);
-        ft.place(200,200);
+        //ft.place(200,200);
+        SLRegularTriangle rt = new SLRegularTriangle(20,20);
+        rt.place(100,100);
+        rt.resize(10);
+
         //SLCircle circle = new SLCircle(20);
         //circle.place(300,300);
         Background bg = new Background();
-        bg.addObjectToScreen(ft);
+        bg.addObjectToScreen(rt);
         //grp.setMinSize(640, 480);
         var scene = new Scene(bg, 640, 480);
         stage.setScene(scene);
@@ -41,10 +51,37 @@ public class Main extends Application
         ft.resizeTransition(2,2);
     }
 
+    @Override
+    public void init()
+    {
+        List<String> params = getParameters().getRaw();
+
+        if(params.size() != 1)
+            //TODO: Handle this.
+            System.err.println("Please provide a valid file.");
+
+        StringBuilder fileContents = new StringBuilder();
+        String line = null;
+        try
+        {
+            File toCompile = new File(params.get(0));
+            BufferedReader reader = new BufferedReader(new FileReader(toCompile));
+            while((line = reader.readLine()) != null)
+            {
+                fileContents.append(line);
+            }
+        }
+        catch(IOException e)
+        {
+            System.err.println("Unable to read supplied file. Please check you have supplied a valid, existing .txt file.");
+            Platform.exit();
+        }
+    }
+
     //LEAVE THIS ALONE!
     public static void main(String[] args)
     {
-        launch();
+        launch(args);
     }
 
 }
