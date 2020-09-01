76feecc5057812186fbe33a13d0e5dec87486efa
Mike <michael.hallam3@gmail.com>
2020-09-01 12:07:42 +0100

    Added time field to StmtType to allow for easier sequencing of StmtType

--- src/main/java/org/shapelang/common/parsercom/StmtType.java
+++ src/main/java/org/shapelang/common/parsercom/StmtType.java
@@ -2,5 +2,11 @@ package org.shapelang.common.parsercom;
 
 public interface StmtType
 {
-	public ParserToken stmtType();
+	ParserToken stmtType(); // allows for correct casting
+
+	/*
+	 * pre: time >= 0
+	 */
+	int time(); // the amount of time the action will take
+	// used for sequencing; 0 is a perfectly acceptable time
 }
