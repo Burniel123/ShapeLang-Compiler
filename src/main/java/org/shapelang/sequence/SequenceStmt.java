235a90e8eadab8ff09ef7f4cb32f8445dddab14b
Mike <michael.hallam3@gmail.com>
2020-09-01 16:44:10 +0100

    Added action 'helper' class in SequenceStmt

--- src/main/java/org/shapelang/sequence/SequenceStmt.java
+++ src/main/java/org/shapelang/sequence/SequenceStmt.java
@@ -10,5 +10,26 @@ import java.util.Optional;
 import java.util.Queue;
 
 public class SequenceStmt {
-    
+    public static Queue<Action> sequence(CanvasInit ci) {
+        final Text head = ci.next;
+        Optional<Text> curMaybe = Optional.of(head);
+
+
+    }
+}
+
+public class Action {
+    final private Twople<StmtType,Integer> act;
+
+    public Action(StmtType stmt, int time) {
+        act = new Twople<>(stmt,time);
+    }
+
+    public StmtType stmt() {
+        return act.fst;
+    }
+
+    public int time() {
+        return act.snd;
+    }
 }
