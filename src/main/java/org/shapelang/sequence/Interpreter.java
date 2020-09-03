686b45532f073b070f1059169795d10f5483281f
Mike <michael.hallam3@gmail.com>
2020-09-03 22:33:30 +0100

    Began the inteppreter. Now has support for Move and Put

--- src/main/java/org/shapelang/sequence/Interpreter.java
+++ src/main/java/org/shapelang/sequence/Interpreter.java
@@ -1,5 +1,43 @@
 package org.shapelang.sequence;
 
+import org.shapelang.common.parsercom.*;
+
+import java.util.Queue;
+
 public class Interpreter {
-    // TODO - interpret the ast
+    public static void interpret(Queue<Action> actQ) {
+        while(!actQ.isEmpty()) {
+            final Action act = actQ.remove();
+            interpret(act);
+
+        }
+    }
+
+    private static void interpret(Action act) {
+        final StmtType stmt = act.stmt();
+        switch(stmt.stmtType()) {
+            case PUT:
+                execPut((Put) stmt);
+                break;
+            case MOVE:
+                execMove((Move) stmt);
+                break;
+            case SEQ:
+                execSeq((Sequential) stmt);
+                break;
+            default: break;
+        }
+    }
+
+    private static void execSeq(Sequential seq) {
+
+    }
+
+    private static void execMove(Move mv) {
+        mv.shapeRef.moveTransition(mv.coord.fst,mv.coord.snd,mv.time());
+    }
+
+    private static void execPut(Put put) {
+        put.shapeRef.place(put.coords.fst,put.coords.snd);
+    }
 }
