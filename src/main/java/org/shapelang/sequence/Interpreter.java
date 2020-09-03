9152f733d7df34ed0a46e0e5c7502c2197d6f72d
Mike <michael.hallam3@gmail.com>
2020-09-03 22:48:05 +0100

    ACTUAL commit to fix conflicts

--- src/main/java/org/shapelang/sequence/Interpreter.java
+++ src/main/java/org/shapelang/sequence/Interpreter.java
@@ -22,7 +22,6 @@ public class Interpreter {
             case MOVE:
                 execMove((Move) stmt);
                 break;
-<<<<<<< HEAD
             case LOOP:
                 break;
             case SEQ:
@@ -33,21 +32,10 @@ public class Interpreter {
                 break;
             case RESIZE:
                 break;
-=======
-            case SEQ:
-                execSeq((SequenceStmt) stmt)
->>>>>>> 7e00c307ce85cb903490da9070ba681a2f5210a7
             default: break;
         }
     }
 
-<<<<<<< HEAD
-=======
-    private static void execSeq(Sequential seq) {
-
-    }
-
->>>>>>> 7e00c307ce85cb903490da9070ba681a2f5210a7
     private static void execMove(Move mv) {
         mv.shapeRef.moveTransition(mv.coord.fst,mv.coord.snd,mv.time());
     }
